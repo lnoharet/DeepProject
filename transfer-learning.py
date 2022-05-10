435ed22fd676182b336fdedd60b4b8f7c948e914
glas444 <99250801+glas444@users.noreply.github.com>
2022-05-10 17:40:04 +0200

    quick search

--- transfer-learning.py
+++ transfer-learning.py
@@ -421,10 +421,11 @@ def main():
             if param.requires_grad == True:
                 print("\t",name)
 
-    dataloaders_dict, dataloaders_dictest = pre_process_dataset(input_size=input_size, subset=1840)
+    dataloaders_dict, dataloaders_dictest = pre_process_dataset(input_size=input_size, subset=10)#=1840)
 
     ### Learning rate search:
-    best_lr = parameter_coarse_to_fine_search(20, model_ft, dataloaders_dict, params_to_update)
+    #best_lr = parameter_coarse_to_fine_search(20, model_ft, dataloaders_dict, params_to_update)
+    best_lr = parameter_coarse_to_fine_search(1, model_ft, dataloaders_dict, params_to_update)
     print("best_lr", best_lr)
     # Observe that all parameters are being optimized
     ## SGD
