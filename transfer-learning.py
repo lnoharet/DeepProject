397ea9f48aee2c23412a68ea1f00d002df9ac05a
cajsap <102965498+cajsap@users.noreply.github.com>
2022-05-19 13:02:03 +0200

    Update transfer-learning.py

--- transfer-learning.py
+++ transfer-learning.py
@@ -254,6 +254,8 @@ def plot(train, val, mode, used_lr, test_acc):
     plt.ylabel(mode)
     plt.title(mode + ' with lr=' + str(used_lr) + ' n_batch=' + str(batch_size) + ' test_acc=' + str(test_acc))
     plt.legend()
+    if mode == "loss":  plt.ylim([0, 1])
+    else:               plt.ylim([0.5, 1])
     plt.savefig('bin_plots/' + mode + str(round(time.time()) - 1600000000) + '.png')
     plt.close()
     return
