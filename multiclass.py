820b74d4b2a208b0ca34aa0083b5dc7bb9a4c854
cajsap <cajsap@kth.se>
2022-05-20 14:44:25 +0200

    LR scheduler update

--- multiclass.py
+++ multiclass.py
@@ -174,9 +174,9 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler = None, num_
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
-            if scheduler:
+            if scheduler and phase == 'train':
                 scheduler.step()
-                
+
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
