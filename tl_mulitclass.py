7aad84b9d8c2b28ab59a89191354047d6ad391b8
lnoharet <78497146+lnoharet@users.noreply.github.com>
2022-05-22 17:07:40 +0200

    merge save load

--- tl_mulitclass.py
+++ tl_mulitclass.py
@@ -250,7 +250,8 @@ def initialize_model(model_name, num_classes, fc_lr = lr_fc, lay4_lr = lr_4, lay
     model_ft.fc = nn.Linear(num_ftrs, num_classes)
     input_size = 224
     params_to_update = []
-    all_layers =[{"params": model_ft.layer1.parameters(), "lr":lay1_lr},{"params": model_ft.layer2.parameters(), "lr":lay2_lr},{"params": model_ft.layer3.parameters(), "lr":lay3_lr},{"params": model_ft.layer4.parameters(), "lr":lay4_lr}, {"params": model_ft.fc.parameters(), "lr":fc_lr}].reverse()
+    all_layers =[{"params": model_ft.layer1.parameters(), "lr":lay1_lr},{"params": model_ft.layer2.parameters(), "lr":lay2_lr},{"params": model_ft.layer3.parameters(), "lr":lay3_lr},{"params": model_ft.layer4.parameters(), "lr":lay4_lr}, {"params": model_ft.fc.parameters(), "lr":fc_lr}]
+    all_layers = all_layers.reverse()
     print(params_to_update)
 
     for l in range(ft_layers):
